2139d04af6de5e84fe1a96a702cf2772da00570a
Miguel Rico <mricofernandez@safareyes.es>
2024-05-20 20:12:15 +0200

    🎨 Improve the structure and create the second view and controller

--- src/main/java/controller/PersonController.java
+++ src/main/java/controller/PersonController.java
@@ -1,14 +1,24 @@
-package com.example.crudjavafx;
+package controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 public class PersonController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private TableView tblPersonas;
+    @FXML
+    private TableColumn colNombre;
+    @FXML
+    private TableColumn colApellidos;
+    @FXML
+    private TableColumn colEdad;
+    @FXML
+    private Button btnAñadir;
+
+
+
 }
---
+++ src/main/java/controller/PersonCreateController.java
@@ -0,0 +1,20 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class PersonCreateController {
+
+    @FXML
+    private Button btnSalir;
+    @FXML
+    private Button btnGuardar;
+    @FXML
+    private TextField txtNombre;
+    @FXML
+    private TextField txtApellidos;
+    @FXML
+    private TextField txtEdad;
+
+}
